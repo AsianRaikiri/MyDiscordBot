3ff71854ddb6fb3f8bbc4c1c37d6a0950f245235
Maik Grunicke <h.m.grunicke@gmail.com>
2022-05-07 18:30:23 +0200

    Added feature to increase sleepcounter by different amounts

--- commands/help.js
+++ commands/help.js
@@ -9,12 +9,12 @@ module.exports = {
         .setTitle('Bob Helper')
         .addField("-ping", '`Just returns pong! back at you. Not really useful eh.`')
         .addField("-help", '`To open the help page, but you are here already so... well.`')
-        .addField("-sleepcounteradd {person}", '`Increments the sleep counter of the person. It does nothing else`')
-        .addField("-sleepstats (optional){person}", '`just returns the times that person has fallen asleep already. \nIf you don\'t specify a person, it returns our stats.`')
+        .addField("-sleepcounteradd {person} (optional){amount}", '`Adds a certain amount to the sleepCounter, if no amount is specified, the sleepCounter is increased by 1. `')
+        .addField("-sleepstats (optional){person}", '`Just returns the times that person has fallen asleep already. \nIf you don\'t specify a person, it returns our stats.`')
         .addField("-sleepleaderboard", '`It returns the ranking of the 3 people who have fallen asleep the most in class, nothing to be proud of honestly.`')
         .addField("-sleepdeleteuser {person}", '`If you grow weary of a person, this is the way to delete that person permanently.`')
         .addField("-currentsemester", '`Gives you the current Semester, or at least the semester, the sleepCounter is currently keeping track of.`')
-        .addField("-changesemester", '`Changes the current semester to a different one. Please mind the Regex: (WS|SS)2[2-9]`')
+        .addField("-changesemester", '`Changes the current semester to a different one. \nPlease mind the Regex: (WS|SS)2[2-9]`')
         message.channel.send({embeds: [botInfo]});
     },
 };
--- commands/sleepcounteradd.js
+++ commands/sleepcounteradd.js
@@ -4,23 +4,32 @@ module.exports = {
     name:'sleepcounteradd',
     description:'Add sleepCounter to a person',
     async execute(message, args, client){
+        let amount;
         let mentionedMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         if(!mentionedMember){return message.channel.send("Which person fell asleep in university?");}
-
+        console.log(args[1])
+        if(args[1] == undefined){
+            amount = 1;
+        }else{
+            amount = args[1];
+        }
+        if(amount < 1){
+            return message.channel.send('This is not a valid number to increase the sleepCounter by.');
+        }
         const levelUser = await Levels.fetch(mentionedMember.user.id, message.guild.id+client.currentSemester, false);
         if(!levelUser){
             message.channel.send(`${mentionedMember.user.username} never fell asleep in university... at least until now.`);
             try{
                 await Levels.createUser(mentionedMember.user.id, message.guild.id+client.currentSemester);
-                await Levels.appendLevel(mentionedMember.user.id, message.guild.id+client.currentSemester, 1);
+                await Levels.appendLevel(mentionedMember.user.id, message.guild.id+client.currentSemester, amount);
 
             }catch(err){
                 console.log(err);
             }
         }else{
             try{
-                await Levels.appendLevel(mentionedMember.user.id, message.guild.id+client.currentSemester, 1);
-                await message.channel.send(`${mentionedMember.user.username} fell asleep ${levelUser.level} times already.`);
+                await Levels.appendLevel(mentionedMember.user.id, message.guild.id+client.currentSemester, amount)
+                await message.channel.send(`${mentionedMember.user.username} fell asleep ${Number(amount) + Number(levelUser.level)} times already.`);
             }catch(err){
                 console.log(err);
         }}
